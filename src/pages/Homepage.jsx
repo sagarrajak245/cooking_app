02d69162921a4fe34aa448607ee326895ac2bee9
sagar rajak <sagarrajak245@gmail.com>
2024-10-19 17:15:51 +0530

    sag

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getRandomColor } from '../lib/utils.js';
 
 import { Search } from "lucide-react";
